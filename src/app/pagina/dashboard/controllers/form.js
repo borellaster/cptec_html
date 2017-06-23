af934ddd02fb9c2b6a0b31e6fef29e586e539f7d
borellaster <borellaster@gmail.com>
2017-06-22 21:13:37 -0300

    ajuste no pastel

--- src/app/pagina/dashboard/controllers/form.js
+++ src/app/pagina/dashboard/controllers/form.js
@@ -5,48 +5,22 @@ define(function(require) {
   module.controller('DashboardCtrl', DashboardCtrl);
   DashboardCtrl.$inject = ['$state', '$stateParams', '$location', 'DashboardFactory', '$timeout',
                            'VariablesResolve', 'IntervalsResolve', 'TypesResolve', 'CitiesFactory', 
-                           'PaginationFactory', 'ModelsResolve', 'YearsResolve', 'MonthsResolve'];
+                           'PaginationFactory', 'ModelsResolve', 'YearsResolve', 'MonthsResolve', 'leafletData'];
   function DashboardCtrl($state, params, $location, dataService, $timeout,
                          variablesResolve, intervalsResolve, typesResolve, dataServiceCity, 
-                         pagination, modelsResolve, yearsResolve, monthsResolve) {
+                         pagination, modelsResolve, yearsResolve, monthsResolve, leafletData ) {
     var vm = this; 
     vm.novo = true;
-    init();   
+    init();  
 
-    vm.pageChanged = function() {
-      pagination.setNextPage(vm.result.page);
-      vm.loadData(pagination.getNextPage());
-    }    
-
-    vm.loadData = function (page) {
-      var latitude = 0;
-      var longitude = 0;
-      if(vm.requisicao.tipoConsulta.val == "CI"){
-        latitude = vm.requisicao.city.latitude;
-        longitude = vm.requisicao.city.longitude;
-      }else if(vm.requisicao.tipoConsulta.val == "CO"){
-        latitude = vm.requisicao.latitude;
-        longitude = vm.requisicao.longitude;
-      }      
-      dataService.listpag(longitude, latitude, getVariables(), 
-                          vm.requisicao.start_month.month,
-                          vm.requisicao.start_year.year, 
-                          vm.requisicao.end_month.month,
-                          vm.requisicao.end_year.year,                           
-                          vm.requisicao.model.id, page, 5)
-        .then(function success(result) {
-        vm.result = result;    
-        vm.currentPage = result.page;
-        pagination.updateMetainf(
-          result.count,
-          result.length,
-          result.page,
-          result.pages
-        );              
-      }).catch(function error(msg) {
-        setError('Erro ao pesquisar os registros.');
-      });      
-    }
+   angular.extend(vm, {
+        center: {
+            lat: -17.518344,
+            lng: -52.207031,
+            zoom: 3
+        }
+    });
+ 
 
     vm.loadCities = function(cidade) {
       if(cidade.length >= 3){
@@ -70,7 +44,7 @@ define(function(require) {
         if (vm.formum.$invalid) {
           return true;
         }
-        vm.loadData(1);
+        //vm.loadData(1);
       }
 
       if (aba == 'tres') {
@@ -210,3 +184,39 @@ define(function(require) {
 
   }
 });
+
+
+    /*vm.pageChanged = function() {
+      pagination.setNextPage(vm.result.page);
+      vm.loadData(pagination.getNextPage());
+    } */   
+
+    /*vm.loadData = function (page) {
+      var latitude = 0;
+      var longitude = 0;
+      if(vm.requisicao.tipoConsulta.val == "CI"){
+        latitude = vm.requisicao.city.latitude;
+        longitude = vm.requisicao.city.longitude;
+      }else if(vm.requisicao.tipoConsulta.val == "CO"){
+        latitude = vm.requisicao.latitude;
+        longitude = vm.requisicao.longitude;
+      }      
+      dataService.listpag(longitude, latitude, getVariables(), 
+                          vm.requisicao.start_month.month,
+                          vm.requisicao.start_year.year, 
+                          vm.requisicao.end_month.month,
+                          vm.requisicao.end_year.year,                           
+                          vm.requisicao.model.id, page, 5)
+        .then(function success(result) {
+        vm.result = result;    
+        vm.currentPage = result.page;
+        pagination.updateMetainf(
+          result.count,
+          result.length,
+          result.page,
+          result.pages
+        );              
+      }).catch(function error(msg) {
+        setError('Erro ao pesquisar os registros.');
+      });      
+    }*/
