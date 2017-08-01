f6e374dd36f95d3124d80f6d4e59c0016414ec3e
borellaster <borellaster@gmail.com>
2017-08-01 18:51:57 -0300

    ajuste no download de arquivos grandes

--- src/app/pagina/dashboard/controllers/download.js
+++ src/app/pagina/dashboard/controllers/download.js
@@ -9,12 +9,49 @@ define(function(require) {
 
     vm.result = resolve;
     
-    vm.download = function () {
+    /*vm.download = function () {
       var file = document.createElement("a");
       var fileName = "Requisicao_"+vm.result.data.id+".zip";
       file.href = "data:application/zip;base64," + vm.result.data.file;
       file.download = fileName;
       file.click();      
-    }
+    }*/
+
+    vm.download = function () {
+        var fileName = "Requisicao_"+vm.result.data.id+".zip";
+        var zipData = vm.result.data.file;
+        var blob = b64toBlob(zipData, "application/zip");
+
+        if (window.navigator.msSaveBlob) {
+            navigator.msSaveBlob(blob, fileName);
+        } else {
+            var link = document.createElement("a");
+            var csvUrl = URL.createObjectURL(blob);
+            link.href = csvUrl;
+            link.style = "visibility:hidden";
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    } 
+
+    function b64toBlob(b64Data, contentType, sliceSize) {
+      contentType = contentType || '';
+      sliceSize = sliceSize || 512;
+      var byteCharacters = atob(b64Data);
+      var byteArrays = [];
+      for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+          byteNumbers[i] = slice.charCodeAt(i);
+        }
+        var byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+      }        
+      var blob = new Blob(byteArrays, {type: contentType});
+      return blob;
+    }       
   }
 });
