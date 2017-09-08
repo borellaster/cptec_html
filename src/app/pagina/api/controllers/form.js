c286cbc72ae7d10e1b55a2b6609943b3e8bc711e
borellaster <borellaster@gmail.com>
2017-09-07 21:40:21 -0300

    tela de API - generate api

--- src/app/pagina/api/controllers/form.js
+++ src/app/pagina/api/controllers/form.js
@@ -3,9 +3,9 @@ define(function(require) {
 
   var module = require('../module');
   module.controller('ApiCtrl', ApiCtrl);
-  ApiCtrl.$inject = ['$state', '$stateParams', '$location', 'ModelsResolve', 'IntervalsResolve', 'MonthsResolve'];
+  ApiCtrl.$inject = ['$state', '$stateParams', '$location', 'ModelsResolve', 'IntervalsResolve', 'MonthsResolve', 'VariablesResolve', 'ConfigurationResolve'];
 
-  function ApiCtrl($state, params, $location, modelsResolve, intervalsResolve, monthsResolve) {
+  function ApiCtrl($state, params, $location, modelsResolve, intervalsResolve, monthsResolve, variablesResolve, configurationsResolve) {
     var vm = this;  
 
     init();
@@ -17,18 +17,51 @@ define(function(require) {
       vm.api.model = vm.models.data[0];
       vm.api.interval = vm.intervals.data[0];
       vm.months = monthsResolve;
+      vm.variables = variablesResolve;
       generateYears(vm.api.model.start_year, vm.api.model.end_year);
       vm.api.start_month = vm.months.data[0]; 
       vm.api.start_year = {'year': vm.api.model.start_year};
       vm.api.end_month = vm.months.data[11];
-      vm.api.end_year = {'year': vm.api.model.end_year};      
+      vm.api.end_year = {'year': vm.api.model.end_year}; 
+      vm.configurations = configurationsResolve;  
+      //vm.link = vm.configurations.data[0].link_api;
     } 
 
     vm.onSelectCallback = function (item){
       generateYears(item.start_year, item.end_year);
       vm.api.start_year = {'year': item.start_year};
       vm.api.end_year = {'year': item.end_year};;
-    };    
+    };
+
+    vm.generateUrl = function (form) {
+      angular.forEach(form.$error, function (field) {
+        angular.forEach(field, function(errorField){
+          console.log(errorField)
+            errorField.$setTouched();
+            errorField.$setDirty();
+        })
+      });
+
+      if (form.$invalid) {
+        setWarning('Informe todos os filtros.');
+        return true;
+      }      
+
+      vm.link = vm.configurations.data[0].link_api;
+      vm.link += "public/";
+      vm.link += vm.api.model.model + "/";
+      vm.link += vm.api.model.id + "/";
+      vm.link += vm.api.interval.nickname + "/";
+      vm.link += vm.api.interval.id + "/";
+      vm.link += vm.api.start_month.month + "/";
+      vm.link += vm.api.start_year.year + "/";
+      vm.link += vm.api.end_month.month + "/";      
+      vm.link += vm.api.end_year.year + "/";
+      vm.link += vm.api.variable.nickname + "/";
+      vm.link += vm.api.latitude + "/";
+      vm.link += vm.api.longitude + "/";
+      
+    }    
 
     function generateYears(startYear, endYear) {
       vm.years = [];
--- src/app/pagina/api/states.js
+++ src/app/pagina/api/states.js
@@ -20,32 +20,52 @@ define(function(require) {
         resolve: { 
           ModelsResolve : modelsList,
           IntervalsResolve: intervalsList,
-          MonthsResolve: monthsList
+          MonthsResolve: monthsList,
+          VariablesResolve: variablesList,
+          ConfigurationResolve: configurationsList
         }
       });
   }
 
   modelsList.$inject = ['ModelsFactory'];
   function modelsList(dataService) {
-      return dataService.combo().then(function success(data) {
-        return data;        
-      }).catch(function error(msg) {
-        setError('Erro ao carregar modelos.')
-      });  
+    return dataService.combo().then(function success(data) {
+      return data;        
+    }).catch(function error(msg) {
+      setError('Erro ao carregar modelos.')
+    });  
   } 
 
   intervalsList.$inject = ['IntervalsFactory'];
   function intervalsList(dataService) {
-      return dataService.combo().then(function success(data) {
-        return data;        
-      }).catch(function error(msg) {
-        setError('Erro ao carregar intervalos.')
-      });
+    return dataService.combo().then(function success(data) {
+      return data;        
+    }).catch(function error(msg) {
+      setError('Erro ao carregar intervalos.')
+    });
   }  
 
   monthsList.$inject = ['DashboardFactory'];
   function monthsList(dataService) {
       return dataService.getMonths();
-  }    
+  }   
+
+  variablesList.$inject = ['VariablesFactory'];
+  function variablesList(dataService) {
+    return dataService.combo().then(function success(data) {
+      return data;
+    }).catch(function error(msg) {
+      setError('Erro ao carregar variáveis.')
+    });
+  }
+
+  configurationsList.$inject = ['ConfigurationsFactory'];
+  function configurationsList(dataService) {
+    return dataService.list(1, 1).then(function success(data) {
+      return data;
+    }).catch(function error(msg) {
+      setError('Erro ao carregar configurações.')
+    });
+  }  
 
 });
